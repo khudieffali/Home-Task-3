7549102eff7125aa13a51a1049a5341966598035
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-03-12 12:20:59 +0400

    new branch and lab task added

--- HomeTaskC#3/Program.cs
+++ HomeTaskC#3/Program.cs
@@ -2,17 +2,19 @@
 
 //Task - 1 Verilmiş array-də tək ədədlərin sayını tapmaq və çap etmək. Məsələn: [1, 2, 3, 4, 5, 6, 7] => Tək ədədlərin sayı: 4
 
-int[] arr = { 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 14 };
-int count = 0;
+//using System.Diagnostics.Metrics;
 
-for (int i = 0; i < arr.Length; i++)
-{
-    if (arr[i] % 2 == 1)
-    {
-        count++;
-    }
-}
-Console.WriteLine("tek ededlerin sayi: " + count);
+//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 14 };
+//int count = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 1)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine("tek ededlerin sayi: " + count);
 #endregion
 
 #region Task 2,3,4
@@ -21,8 +23,9 @@ Console.WriteLine("tek ededlerin sayi: " + count);
 //Task - 2 Verilmiş array-də ən kiçik və ən böyük ədədlərin cəmini tapmaq. Məsələn: [3, 8, 2, 10, 5] => Cəm: 12
 //Task - 3 Verilmiş array-dəki ədədlərin ən böyük və ən kiçik fərqlərini tapmaq. Məsələn: [5, 8, 15, 3, 7] => Fərq: 12
 //Task - 4 Verilmiş array-dəki ədədlərin sırasını artan sıraya salmaq. Məsələn: [3, 1, 5, 4, 2] => [1, 2, 3, 4, 5]
-//int[] arr = { 3, 8, 2, 10, 5 };
+//int[] arr = { 1,9,3,8,5,4 };
 //int num;
+//int count=0;
 
 //for (int i = 0; i < arr.Length; i++)
 //{
@@ -35,6 +38,7 @@ Console.WriteLine("tek ededlerin sayi: " + count);
 //            arr[j] = num;
 //        }
 //    }
+
 //}
 //Task-2
 //Console.WriteLine(arr[0] + arr[arr.Length - 1]);
@@ -69,6 +73,50 @@ Console.WriteLine("tek ededlerin sayi: " + count);
 
 #endregion
 
+
+
+#region Task Lab1
+
+//int[] arr = { 5, 8, 3, 12, 11, 10 };
+//int count=0;
+//int sum=0;
+//int avarage;
+//foreach (var item in arr)
+//{
+//    sum+= item;
+//    count++;
+//}
+//avarage= sum/count;
+//Console.WriteLine("Ededlerin cemi: "+sum);
+//Console.WriteLine("Ededlerin ededi ortasi: "+avarage);
+#endregion
+
+#region Task Lab2
+
+//int k=0;
+//int k1=0;
+//int k2 = 0;
+
+//Console.WriteLine("1 ci noqtenin x ve y deyerlerini daxil edin:");
+//int x= Convert.ToInt32(Console.ReadLine());
+//int y = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("2 ci noqtenin x ve y deyerlerini daxil edin:");
+//int x1 = Convert.ToInt32(Console.ReadLine());
+//int y1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("3 ci noqtenin x ve y deyerlerini daxil edin:");
+//int x2 = Convert.ToInt32(Console.ReadLine());
+//int y2 = Convert.ToInt32(Console.ReadLine());
+
+//k =( y1 - y) / (x1 - x);
+//k1 = (y2 - y1) / (x2 - x1);
+
+//if (k == k1 )
+//{
+//    Console.WriteLine("Hamisi bir xettin uzerine dusur");
+//}
+#endregion
 
 
 
--- HomeTaskC#3/obj/Debug/net6.0/HomeTaskC#3.AssemblyInfo.cs
+++ HomeTaskC#3/obj/Debug/net6.0/HomeTaskC#3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeTaskC#3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16531437af05745a3268e16ac5325d566e2af5b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b084cc7c38c7b9c4a8f21743481910f94c73bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeTaskC#3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeTaskC#3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
