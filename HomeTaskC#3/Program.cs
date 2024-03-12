6b084cc7c38c7b9c4a8f21743481910f94c73bb1
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-03-12 10:45:27 +0400

    changed

--- HomeTaskC#3/Program.cs
+++ HomeTaskC#3/Program.cs
@@ -2,17 +2,17 @@
 
 //Task - 1 Verilmiş array-də tək ədədlərin sayını tapmaq və çap etmək. Məsələn: [1, 2, 3, 4, 5, 6, 7] => Tək ədədlərin sayı: 4
 
-//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 14 };
-//int count = 0;
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 14 };
+int count = 0;
 
-//for (int i = 0; i < arr.Length; i++)
-//{
-//    if (arr[i] % 2 == 1 && arr[i] != 1)
-//    {
-//        count++;
-//    }
-//}
-//Console.WriteLine("tek ededlerin sayi: " + count);
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 1)
+    {
+        count++;
+    }
+}
+Console.WriteLine("tek ededlerin sayi: " + count);
 #endregion
 
 #region Task 2,3,4
--- HomeTaskC#3/obj/Debug/net6.0/HomeTaskC#3.AssemblyInfo.cs
+++ HomeTaskC#3/obj/Debug/net6.0/HomeTaskC#3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeTaskC#3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16531437af05745a3268e16ac5325d566e2af5b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeTaskC#3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeTaskC#3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
